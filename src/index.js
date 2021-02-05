9346364d98c727fb058fb387570175fab04138bb
eva-mist <66295847+eva-mist@users.noreply.github.com>
2021-02-05 01:47:22 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  // take input
+const string = prompt('Enter a string: ');
+
+// call the function
+const value = checkPalindrome(string);
+
+console.log(value);
+
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+
+    // find the length of a string
+    const len = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+
 }
 
 function solution(arg) {
